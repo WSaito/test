6834eda07614b28e511b8884a69840f6764e4a9e
Saito <saito@Saito-no-Mac-Pro.local>
2016-02-04 08:41:38 +0900

    change greeting

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	printf("Hello everyone\n");
 	printf("Hello Hello everyone\n");
-	printf("Good evening from work branch\n");
+	printf("Good afternoon from work branch\n");
 	return 0;
 }
 
