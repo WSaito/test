cd208f98f3d782e09cfc98d1391d3889fa20a2c4
Saito <saito@Saito-no-Mac-Pro.local>
2016-02-04 10:12:42 +0900

    aaaaa

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	printf("Hello everyone\n");
 	printf("Good afternoon from work branch\n");
+	printf("Hello everyone\n");
+	printf("Good evening2 from work branch\n");
 	return 0;
 }
 
